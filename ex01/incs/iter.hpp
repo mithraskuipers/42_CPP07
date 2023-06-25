c92521bffeab3114fbde3476e66459a887e7201d
Mithras Kuipers <mithraskuipers@gmail.com>
2023-06-25 22:20:46 +0200

    update

--- ex01/incs/iter.hpp
+++ ex01/incs/iter.hpp
@@ -6,13 +6,31 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/25 19:24:54 by mikuiper      #+#    #+#                 */
-/*   Updated: 2023/06/25 19:25:52 by mikuiper      ########   odam.nl         */
+/*   Updated: 2023/06/25 22:09:05 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <cstddef> // for size_t
+#include <iostream> // for cout and endl
 
+// Function template iter
+template <typename T, typename Func>
+void iter(T* arr, size_t len, Func func)
+{
+	for (size_t i = 0; i < len; ++i)
+	{
+		func(arr[i]);
+	}
+}
+
+// Example function to be called on each array element
+template <typename T>
+void printElement(T element)
+{
+	std::cout << element << std::endl;
+}
 
 #endif
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,11 +6,35 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/23 20:23:30 by mikuiper      #+#    #+#                 */
-/*   Updated: 2023/06/23 20:23:30 by mikuiper      ########   odam.nl         */
+/*   Updated: 2023/06/25 22:20:15 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./../incs/iter.hpp"
+
 int main()
 {
+	std::cout << std::endl;
+	std::cout << "Example of integer array:" << std::endl;
+	int intArray[] = {1, 2, 3, 4, 5};
+	size_t intArrayLen = sizeof(intArray) / sizeof(intArray[0]);
+	iter(intArray, intArrayLen, printElement<int>);
 
+	std::cout << std::endl;
+	std::cout << "Example of float array:" << std::endl;
+	float floatArray[] = {1.123, 2.234, 3.345, 4.456, 5.567};
+	size_t floatArrayLen = sizeof(floatArray) / sizeof(floatArray[0]);
+	iter(floatArray, floatArrayLen, printElement<float>);
+
+	std::cout << std::endl;
+	std::cout << "Example of character array:" << std::endl;
+	char charArray[] = {'a','b','c','d','e'};
+	size_t charArrayLen = sizeof(charArray) / sizeof(charArray[0]);
+	iter(charArray, charArrayLen, printElement<char>);
+
+	std::cout << std::endl;
+	std::cout << "Example of string array:" << std::endl;
+	std::string stringArray[] = {"Hoi", "ik", "ben", "Mithras"};
+	size_t stringArrayLen = sizeof(stringArray) / sizeof(stringArray[0]);
+	iter(stringArray, stringArrayLen, printElement<std::string>);
 }
